f75b8889b93aa99442651ce334c58c3f00b4d38b
Aaron Burdick <burdick.aaron@gmail.com>
2016-12-28 23:55:11 +0900

    1. Small fix to _lists.scss. 2. Finished data modeling for hero and about sections.

---
+++ app/data/cross-image.js
@@ -0,0 +1,49 @@
+export default {
+	id: "cross",
+	name: "Cross",
+	url: "http://zakirinfo.com/cross/image.html",
+	
+	sections: {
+		hero: {
+			text: {
+				title: "Unique and Modern Design",
+				subTitle: "Multipurpose HTML Template",
+				description: "Nam liber tempor cum soluta nobis eleifend option congue nihil imperdiet
+			doming id quod mazim placerat facer possim assum."			
+			},
+			cta: {
+				text: 'get started now',
+				target: '#about-section'
+			}
+		},
+		about: {
+			title: "We are digital agency based in Australia. sed do eiusmod
+tempor incididunt ut labore .",
+			subtitle: "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex.",
+			features: [
+				{
+					title: "Handmade Layouts",
+					description: "Nam liber tempor cum soluta nobis eleifend option congue nihil."
+				},
+				{
+					title: "Unlimited User",
+					description: "Nam liber tempor cum soluta nobis eleifend option congue nihil."
+				},
+				{
+					title: "Extriem Settings",
+					description: "Nam liber tempor cum soluta nobis eleifend option congue nihil."
+				},
+				{
+					title: "Unlimited Update",
+					description: "Nam liber tempor cum soluta nobis eleifend option congue nihil."
+				}
+			]
+		},
+		service: {},
+		portfolio: {},
+		testimonial: {},
+		blog: {},
+		price: {},
+		contact: {}
+	}
+}
