901f1f1b0a111346bf67c59d5c3f8baf810d32a5
Aaron Burdick <burdick.aaron@gmail.com>
2017-01-02 16:20:38 +0900

    Moved cross-image.js into templates, added data file to global.js

--- app/data/global.js
+++ app/data/global.js
@@ -3,6 +3,7 @@ import footer from './components/footer';
 
 import index from './templates/index';
 import templateName from './templates/template-name';
+import crossImage from './templates/cross-image';
 
 const global = {
   title: "Global Site Title",
